a1b96fd735b85302ffe796753fe99db012efea62
brian.bi <brian.bun@outlook.com>
2017-08-21 17:06:08 +0800

    suport C2

---
+++ Model/Quote/Address/Validator.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace Yaoli\Paypals\Model\Quote\Address;
+use Zend_Validate_Exception;
+
+class Validator extends Magento\Quote\Model\Quote\Address\Validator
+{
+	public function isValid($value)
+    {
+        $messages = [];
+        $email = $value->getEmail();
+        if (!empty($email) && !\Zend_Validate::is($email, 'EmailAddress')) {
+            $messages['invalid_email_format'] = 'Invalid email format';
+        }
+
+        $countryId = $value->getCountryId() == 'C2' ? 'CN' : $value->getCountryId();
+
+        if (!empty($countryId)) {
+            $country = $this->countryFactory->create();
+            $country->load($countryId);
+            if (!$country->getId() && $countryId != 'CN') {
+                $messages['invalid_country_code'] = 'Invalid country code';
+            }
+        }
+
+        $this->_addMessages($messages);
+
+        return empty($messages);
+    }
+}
